be970de67cd92bb50e00fb42ac5e0627ca17d500
Maor Bernstein <maor@maor-Latitude-E6430.(none)>
2014-03-03 03:37:59 -0500

    Fixing up the last file

--- hw5/levenshtein_fun.py
+++ hw5/levenshtein_fun.py
@@ -5,46 +5,19 @@ Created on Sat Mar  1 17:53:00 2014
 @author: gabrielle
 """
 import re
-esp = open('Esperanto.txt','r')
-esp = esp.read()
-w = open('Welsh.txt', 'r')
-w = w.read()
+import pickle
 g = open('German.txt','r')
-g = g.read()
-oe = open('OldEnglish.txt','r')
-oe = oe.read()
-esptag = open('EnEspTag.txt','r')
-esptag = esptag.read()
-
-
-def get_lingual_distance(book):
-    #book is a translation dictionary
-    #word_pair_list contains a list of lists for each word defined in the dictionary
-    word_pair_list = parse_word_pairs(book)
-    distances = list()
-    num_subst = 0
-    num_ins = 0
-    num_del = 0
-    for i in range(len(word_pair_list)):
-        for j in range(len(word_pair_list[i])):
-            output = levenshtein_fun(word_pair_list[i][0],word_pair_list[i][j])
-            distances.append(output[0])
-            num_subst += output[1]
-            num_ins += output[2]
-            num_del += output[3]
-    return output
-            
+german = g.read()
+g.close()
+f = open('LengthbySize.pickle','w')
 def parse_word_pairs(book):
-    if book==esp:
-        word_list = re.split('\n',esp)
-        for i in range(len(word_list)):
-            line = re.split(' = ',word_list[i])
-            word_list[i] = []
-            word_list[i].append(line[0].split()[0].strip(','))
-            word_list[i].extend(line[1].split()) 
-        return word_list
-        
-print parse_word_pairs(esp)[0]
+    word_list = re.split('\r\n\r\n',book)
+    word_list.remove('\xef\xbb\xbf')
+    pairs = []
+    for i in range(len(word_list)):
+        s = re.split('  +',word_list[i])
+        pairs.append((s[0].split(),s[1].split()))
+    return pairs
 
 def levenshtein_fun(s1, s2):
     num_subst = 0
@@ -69,17 +42,49 @@ def levenshtein_fun(s1, s2):
             #transforming a string by insertion has an edit distance of 1
             insertions = prior_row[j + 1] + 1
             #add 1 to the edit distance between the prior c2 and the same c1
-            deletions = current_row[j] +1
+            deletions = current_row[j] + 1
             #add one to the edit distance between the prior c2 and the prior c1
-            substitutions = prior_row[j] + 1
+            substitutions = prior_row[j] + (c1 != c2)
             current_row.append(min(insertions, deletions, substitutions))
-            if min(insertions, deletions, substitutions) == substitutions:
-                num_subst+=1
-            elif min(insertions, deletions, substitutions) == insertions:
-                num_ins+=1
-            else:
-                num_del+=1
         prior_row = current_row
-    return prior_row, num_subst, num_ins, num_del
+        weighted_dist = float(prior_row[-1])/float(len(s1))
+    return weighted_dist
+
+def get_lingual_distance(book):
+    #book is a translation dictionary
+    #word_pair_list contains a list of lists for each word defined in the dictionary
+    #the output is a long string that contains the levenshtein distance, two spaces, and then the length of the string
+    #This is then put in matlab and plotted
+    word_pair_list = parse_word_pairs(book)
+    distances = list()
+    num_subst = 0
+    num_ins = 0
+    num_del = 0
+    out = []
+    for i in range(len(word_pair_list)):
+        dist =  []
+        leg = []
+        german = word_pair_list[i][0]
+        english = word_pair_list[i][1]
+        for j in range(len(german)):
+            for k in range(len(english)):
+                dist.append(levenshtein_fun(german[j],english[k]))
+                leg.append(max([len(german[j]) , len(english[k])]))
+        out.append([str(min(dist)),str(max(leg))])
+        outstring = listtostr(out)
+        pickle.dump("apples",f)
+        f.close()
+
+def listtostr(s):
+    string = ""
+    for i in range(len(s)):
+        for j in range(len(s[i])):
+            string = string + s[i][j] + '  '
+        string = string + '\n'
+    return string 
+
+
+
+
+
             
-        
